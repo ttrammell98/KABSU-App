60b789de9427297541063a4b98f02f55898eea4f
Grady Salzman <gradysalzman@ksu.edu>
2019-12-11 10:54:12 -0600

    Added Procudure

---
+++ SQL/query.sql
@@ -0,0 +1,61 @@
+
+-- procudere to query informaion based on condistions from spcified in front end
+CREATE OR ALTER PROCEDURE kabsu.RetriveData
+@Owner NVARCHAR(128),
+@Breed NVARCHAR(128),
+@AnimalName NVARCHAR(128),
+@Code NVARCHAR(128),
+@CanNum NVARCHAR(128),
+@Town NVARCHAR(128),
+@State NVARCHAR(128),
+
+AS
+
+
+SELECT S.Valid, S.CanNum, A.AnimalID, A.CollDate, S.NumUnites, A.Name, A.Breed, A.RegNum, P.Name, P.Town, P.State
+FROM Sample S 
+INNER JOIN Person P ON P.PersonID = S.PersonID
+INNER JOIN Animal A ON A.AnimalID = A.AnimalID
+WHERE P.Name = CASE @Owner
+			WHEN N'*'
+			THEN 
+				P.Name
+			ELSE
+				@Owner
+			end
+AND A.Breed = CASE @Breed
+			WHEN N'*'
+			THEN
+				P.Name
+			ELSE
+				@Breed
+			END
+AND A.Name = CASE @AnimalName
+			WHEN N'*'
+			THEN
+				A.Breed
+			ELSE
+				@AnimalName
+			END
+AND A.AnimalID = CASE @Code
+			WHEN N'*'
+			THEN
+				A.AnimalID
+			ELSE
+				@Code
+			END
+AND S.CanNum= CASE @CanNum
+			WHEN N'*'
+			THEN
+				S.CanNum
+			ELSE
+				@CanNum
+			END
+AND P.Town = CASE @Town
+			WHEN N'*'
+			THEN
+				P.Town
+			ELSE
+				@Town
+			END
+GO
