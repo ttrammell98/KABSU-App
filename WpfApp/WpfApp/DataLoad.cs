b9d44ddcf0ebd18737b5b83c699f8ebe7d9fc9b4
daleyk <daleyk@ksu.edu>
2020-02-11 16:46:17 -0600

    Created DataLoad.cs to assist in rebuilding the database (and some small changes that happened automatically

---
+++ WpfApp/WpfApp/DataLoad.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Class to automate filling the database while providing an output of failed entries
+    /// </summary>
+    class DataLoad
+    {
+        /// <summary>
+        /// Primary method to be called
+        /// </summary>
+        public void DatabaseLoad()
+        {
+            using (StreamReader sr = new StreamReader("C:/Users/e10d1/source/repos/KABSU - Inventory - App/People.csv"))
+            {
+                string line;
+                string[] lineTokens;
+
+                while ((line = sr.ReadLine()) == null)
+                {
+                    // Break down line into its constituent parts
+                    lineTokens = line.Split(',');
+
+                    // Add entry to database
+
+                }
+            }
+        }
+    }
+}
