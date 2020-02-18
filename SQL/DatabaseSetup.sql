ecfeee7abafd73b56facc009f6e8456a8619722c
shant haik <shant@ksu.edu>
2020-02-18 16:46:52 -0600

    shows records now

--- SQL/DatabaseSetup.sql
+++ SQL/DatabaseSetup.sql
@@ -29,6 +29,16 @@ PersonID INT NOT NULL,
 Notes VARCHAR(1000)
 );
 
+CREATE TABLE Record (
+AnimalID VARCHAR(32) NOT NULL,
+ToFrom VARCHAR(100) NOT NULL,
+Date VARCHAR(32) NOT NULL,
+NumReceived INT NOT NULL,
+NumShipped INT NOT NULL,
+Balance INT NOT NULL,
+FOREIGN KEY (AnimalID) REFERENCES kabsu.animal (AnimalID)
+);
+
 INSERT INTO Person (Name, City, State, Country)
 VALUES ('Mouse, Mickey', 'Beloit', 'KS', 'USA');
 
