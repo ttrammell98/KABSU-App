3d5d7056118d5945d8be730d58b6e7e74e046fe1
daleyk <daleyk@ksu.edu>
2020-02-18 16:48:03 -0600

    Started writing insert functions

---
+++ WpfApp/WpfApp/InsertData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+
+namespace WpfApp
+{
+    class InsertData
+    {
+        public void InsertPerson(int personID, string name, string city, string state, string country)
+        {
+            string connectionString = "Server=localhost;Database=kabsu; User ID = appuser; Password = test; Integrated Security=true";
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+    }
+}
