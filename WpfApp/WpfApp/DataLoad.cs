6e996dc633d1e80d8e830ca5966dcb67032b0513
daleyk <daleyk@ksu.edu>
2020-02-27 23:01:09 -0600

    Updated DataLoad and InsertData so that people can be added from .csv

--- WpfApp/WpfApp/DataLoad.cs
+++ WpfApp/WpfApp/DataLoad.cs
@@ -15,20 +15,31 @@ namespace WpfApp
         /// <summary>
         /// Primary method to be called
         /// </summary>
-        public void DatabaseLoad()
+        public static void DatabaseLoad()
         {
-            using (StreamReader sr = new StreamReader("C:/Users/e10d1/source/repos/KABSU - Inventory - App/People.csv"))
+            using (StreamReader sr = new StreamReader("C:/Users/e10d1/source/repos/KABSU-Inventory-App/Database Files/Database Sample Data/People.csv"))
             {
                 string line;
                 string[] lineTokens;
+                string name;
+                StringBuilder errorList = new StringBuilder();
 
-                while ((line = sr.ReadLine()) == null)
+                line = sr.ReadLine();
+
+                while ((line = sr.ReadLine()) != null)
                 {
+                    name = "";
+
                     // Break down line into its constituent parts
+                    if (line.Split(new string[] { "\"" }, StringSplitOptions.None).Count() > 1) 
+                        name = line.Split(new string[] {"\""}, StringSplitOptions.None)[1];
                     lineTokens = line.Split(',');
 
                     // Add entry to database
-
+                    if (name == "")
+                        errorList.Append(InsertData.InsertPerson(Convert.ToInt32(lineTokens[0]), lineTokens[1], lineTokens[2], lineTokens[3], lineTokens[4]));
+                    else
+                        errorList.Append(InsertData.InsertPerson(Convert.ToInt32(lineTokens[0]), name, lineTokens[3], lineTokens[4], lineTokens[5]));
                 }
             }
         }
--- WpfApp/WpfApp/InsertData.cs
+++ WpfApp/WpfApp/InsertData.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using MySql.Data.MySqlClient;
-using System.Data.SqlClient;
+using System.Windows;
+
 
 namespace WpfApp
 {
     class InsertData
     {
-        public void InsertPerson(int personID, string name, string city, string state, string country)
+        public static string InsertPerson(int personID, string name, string city, string state, string country)
         {
             string connectionString = "Server=localhost;Database=kabsu; User ID = appuser; Password = test; Integrated Security=true";
             try
@@ -26,11 +29,52 @@ namespace WpfApp
                         command.Parameters.AddWithValue("@City", city);
                         command.Parameters.AddWithValue("@State", state);
                         command.Parameters.AddWithValue("@Country", country);
+
+                        connection.Open();
+
+                        var reader = command.ExecuteReader();
+
+                        reader.Read();
+                    }
+                }
+                return "";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+        }
+
+        public void InsertPerson(string name, string city, string state, string country)
+        {
+            string connectionString = "Server=localhost;Database=kabsu; User ID = appuser; Password = test; Integrated Security=true";
+            try
+            {
+                using (var connection = new MySqlConnection(connectionString))
+                {
+                    using (var command = new MySqlCommand("kabsu.InsertPerson", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("@Name", name);
+                        command.Parameters.AddWithValue("@City", city);
+                        command.Parameters.AddWithValue("@State", state);
+                        command.Parameters.AddWithValue("@Country", country);
+
+                        connection.Open();
+
+                        var reader = command.ExecuteReader();
+
+                        while (reader.Read())
+                        {
+
+                        }
                     }
                 }
             }
             catch (Exception e)
             {
+                MessageBox.Show(e.Message);
 
             }
         }
