e57c63200d06cf318f5873f3512c15e8a9fa1957
ttrammell98 <ttrammell98@ksu.edu>
2020-04-09 15:59:18 -0500

    Added Testing Framework

---
+++ WpfApp/KABSUApp_UnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace KABSUApp_UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ WpfApp/KABSUApp_UnitTest/UnitTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using WpfApp;
+
+namespace KABSUApp_UnitTest
+{
+    [TestFixture]
+    public class UnitTests
+    {
+
+        [SetUp]
+        public void setup()
+        {
+
+        }
+
+        [TearDown]
+        public void tearDown()
+        {
+
+        }
+
+        [Test]
+        public void sampleTest()
+        {
+
+        }
+
+
+        [Test]
+        public void test_searchValidOwner()
+        {
+            SearchTerm search = new SearchTerm("0658", "54XB445", "1 Oak", "Test", "Mouse Mick", "Beloit", "KS"); //actual entry
+            SearchResults results = new SearchResults();
+            List<SearchResult> data = results.retrieveData(search);
+            Assert.AreEqual(2, data.Count);
+        }
+    }
+}
